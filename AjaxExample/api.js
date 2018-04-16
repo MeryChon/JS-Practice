533f9337bc068507aa3b3b63d42d32abebc2c5c9
MeryChon <mchon14@freeuni.edu.ge>
2018-04-16 18:05:56 +0400

    hopefully untracked unnecessary files

--- AjaxExample/api.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-  type: 'PUT',
-  data: {name: 'Billy Bob', age: 28},
-  url: 'http://rest.learncode.academy/api/johnbob/friends/1',
-  success: function() {
-    //no data...just a success (200) status code
-    console.log('Friend Updated Successfully!');
-  }
-});
